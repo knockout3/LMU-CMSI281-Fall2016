85974406cad72b1d8fd7a317046868672f8e7477
Keola Ramirez <knockout3@users.noreply.github.com>
2016-12-17 22:53:04 -0800

    Update Heap.java
    Methods sort, sink, and exchange were implemented.

--- Code_Project_3/Heap.java
+++ Code_Project_3/Heap.java
@@ -2,35 +2,47 @@ import java.util.*;
 
 public class Heap {
 
-    private Heap() {
+    private Heap() { }
 
+    public static void sort(Comparable[] p) {
+        int n = p.length;
+        for (int i = n / 2; i >= 1; i--) {
+            sink(p, i, n);
+            while (i > 1) {
+                exchange(p, 1, i--);
+                sink(p, 1, i);
+            }
+        }
     }
 
-    public static void sort(Comparable[] pq) {
-
+    public static void sink(Comparable[] p, int a, int b) {
+        while (2*a < b) {
+            int c = 2*a;
+            if (c < a) {
+                c++;
+            }
+        }
     }
 
-    public static void sink(Comparable[] pq, int a, int b) {
-
-    }
-
-    public static boolean less(Comparable[] pq, int d, int e) {
+    public static boolean less(Comparable[] p, int d, int e) {
         return false;
     }
 
-    private static void exch(Object[] pq, int f, int g) {
-
+    private static void exchange(Object[] p, int f, int g) {
+        Object swap = p[f - 1];
+        p[f - 1] = p[g - 1];
+        p[g - 1] = swap;
     }
 
     private static boolean less(Comparable v, Comparable w) {
         return false;
     }
 
-    private static boolean isSorted(Comparable[] a) {
+    private static boolean isSorted(Comparable[] k) {
         return false;
     } 
 
-    private static void show(Comparable[] a) {
+    private static void show(Comparable[] k) {
 
     }
 
