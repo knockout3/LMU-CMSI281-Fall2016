937d9fb091795356f4468b07d626dd91dd705aa6
Keola Ramirez <knockout3@users.noreply.github.com>
2016-12-17 22:45:29 -0800

    Create Heap.java
    Creation of Heap program with non-implemented functions

---
+++ Code_Project_3/Heap.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Heap {
+
+    private Heap() {
+
+    }
+
+    public static void sort(Comparable[] pq) {
+
+    }
+
+    public static void sink(Comparable[] pq, int a, int b) {
+
+    }
+
+    public static boolean less(Comparable[] pq, int d, int e) {
+        return false;
+    }
+
+    private static void exch(Object[] pq, int f, int g) {
+
+    }
+
+    private static boolean less(Comparable v, Comparable w) {
+        return false;
+    }
+
+    private static boolean isSorted(Comparable[] a) {
+        return false;
+    } 
+
+    private static void show(Comparable[] a) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
